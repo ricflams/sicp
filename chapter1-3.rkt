c441bc86b077dda13d99ade191287ad86cebce77
Richard Flamsholt <richard@flamsholt.dk>
2025-01-13 16:51:12 +0100

    cube-root

--- chapter1-3.rkt
+++ chapter1-3.rkt
@@ -46,3 +46,47 @@
 ; (cube-root 100000000000000000)
 ; (cube-root 1.2)
 ; (cube-root 0.001)
+
+(define (raise x n)
+	(if (= n 0)
+		1
+		(* x (raise x (- n 1)))))
+(raise 3 0)
+(raise 3 1)
+(raise 3 2)
+(raise 3 3)
+(raise 3 4)
+
+(define (root x power improve)
+	(define (iter guess)
+		(if (good-enough? guess)
+			guess
+			(iter (improve guess))
+		))
+	(define (good-enough? guess)
+		(< (abs (- x (raise guess power))) (/ guess 100000000)))
+	(iter 1.0))
+
+(define (root-2 x)
+	(define (improve guess)
+		(average guess (/ x guess)))
+	(root x 2 improve))
+
+"root-2"
+(root-2 9)
+(root-2 1000000)
+(root-2 10000000000)
+(root-2 10000000000000000000000000)
+(root-2 1.2)
+(root-2 0.001)
+
+
+(define (root-3 x)
+	(define (improve guess)
+		(/ (+ (/ x (* guess guess)) (* 2 guess)) 3))
+	(root x 3 improve))
+
+"root-3"
+(root-3 27)
+(root-3 1000000)
+(root-3 10000000000)	
