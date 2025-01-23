cd8a2508f5c30fff289cc4ddac12a3fe8923bc71
Richard Flamsholt <richard@flamsholt.dk>
2025-01-24 00:19:50 +0100

    1.22

--- exercise-1-22.rkt
+++ exercise-1-22.rkt
@@ -6,21 +6,44 @@
 ;; 1.22
 
 (define (timed-prime-test n)
-  (newline)
-  (display n)
   (start-prime-test n (runtime)))
 
 (define (start-prime-test n start-time)
   (if (prime? n)
-      (report-prime (- (runtime) start-time))))
+      (report-prime n (- (runtime) start-time))
+      #f))
 
-(define (report-prime elapsed-time)
+(define (report-prime n elapsed-time)
+  (display n)
   (display " *** ")
-  (display elapsed-time))
+  (display (/ elapsed-time 1000.0))
+  (newline)
+  #t)
 
-;; (define (test-for-primes start n)
-;;   (if (> n 0)
-;;       (search-for-primes (next-prime-pos start
+
+
+(define (test-for-primes start count)
+  (cond ((even? start)
+	 (test-for-primes (+ start 1) count))
+	((> count 0)
+	 (test-for-primes
+	  (+ 2 (find-next-prime start))
+	  (- count 1)))))
+
+(define (find-next-prime x)
+  (if (timed-prime-test x)
+      x
+      (find-next-prime (+ x 2))))
+
+
+(test-for-primes 100000000 3)
+(test-for-primes 1000000000 3)
+(test-for-primes 10000000000 3)
+(test-for-primes 100000000000 3)
+(test-for-primes 1000000000000 3)
+(test-for-primes 10000000000000 3)
+
+
 
 
 
