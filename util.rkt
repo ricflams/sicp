229ff545b0a708e59360b4ce645d88de51dcecd0
Richard Flamsholt <richard@flamsholt.dk>
2025-02-09 00:33:02 +0100

    add print-eval-verify

--- util.rkt
+++ util.rkt
@@ -15,8 +15,22 @@
 (define-syntax print-eval
   (syntax-rules ()
     [(print-eval expr)
-     (begin
-       (print-with-parenthesis
-	(lambda ()
-	  (print 'expr " -> " expr))))]))
+     (print-with-parenthesis
+      (lambda ()
+	(print 'expr " -> " expr)))]))
 
+
+(#%provide print-eval-verify)
+(define-syntax print-eval-verify
+  (syntax-rules ()
+    [(print-eval-verify expr expected)
+     (print-with-parenthesis
+      (lambda ()
+	(let ((actual expr))
+	  (define (are-equal a b)
+	    (if (boolean? a)
+		(or (and a b) (and (not a) (not b)))
+		(= a b)))
+	  (if (are-equal actual expected)
+	      (print 'expr " -> " actual " = " expected)
+	      (print "\n" 'expr " -> " actual " IS NOT EQUAL TO " expected " ########\n")))))]))
