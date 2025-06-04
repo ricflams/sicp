664fa327dfd61ec09cde6d3c245a319cefdc4ef3
Richard Flamsholt <richard@flamsholt.dk>
2025-06-04 21:32:24 +0200

    fixed typo

--- 2.3.rkt
+++ 2.3.rkt
@@ -334,7 +334,7 @@
 	((null? set2) set1)
 	((element-of-set? (car set1) set2)
 	 (union-set (cdr set1) set2))
-	(else (cons (caro set1) (union-set (cdr set1) set2)))))
+	(else (cons (car set1) (union-set (cdr set1) set2)))))
 
 (print-eval (union-set '(1 2 3) '(4 5 6)))
 (print-eval (union-set '(1 2 3) '()))
